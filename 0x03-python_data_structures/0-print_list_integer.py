3a50b2402506d72477ff495e4a8def19985b9e42
Fedora-Joyce <101561702+Fedora-Joyce@users.noreply.github.com>
2022-07-04 06:57:38 -0700

    Delete 0-print_list_integer.py

--- 0x03-python_data_structures/0-print_list_integer.py
+++
@@ -1,7 +0,0 @@
-Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-#!/usr/bin/python3
-
-def print_list_integer(my_list=[]):
-    for j in range(len(my_list)):
-        print("{:d}".format(my_list[j]))
