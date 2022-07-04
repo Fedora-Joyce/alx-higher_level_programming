1096f70568ed6ba1bb1799e8afe146ab616a77ee
Fedora-Joyce <101561702+Fedora-Joyce@users.noreply.github.com>
2022-07-03 23:42:15 -0700

    task

---
+++ 0x03-python_data_structures/0-main.py
@@ -0,0 +1,7 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#!/usr/bin/python3
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
