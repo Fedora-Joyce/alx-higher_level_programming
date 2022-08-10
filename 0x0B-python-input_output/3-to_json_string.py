1c23e12a9a675c60be27a00d7daa544016555e10
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-08-10 19:30:14 +0000

    updates

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    The 3-to_json_string module
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ serializes an obj to its string representation """
+
+    return (json.dumps(my_obj))
