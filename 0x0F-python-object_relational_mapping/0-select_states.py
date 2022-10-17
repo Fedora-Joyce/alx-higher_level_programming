225736d3528179a3aa8466ccb4c611f8d37c07e6
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-10-17 20:58:24 +0100

    O-RPM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""
-file: 0-select_states.py
-Desc: This module contains a script to  lists all
-states from the database hbtn_0e_0_usa
-Author: Gizachew Bayness (Elec Crazy).
-Date Created: oct 7, 2022
-"""
+""" The 0-select_states.py module """
 
-from sys import argv
-import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states_info = cur.fetchall()
-
-    for state in states_info:
-        print(state)
+    from sys import argv
+    import MySQLdb
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3])
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+    except Exception as err:
+        print(err)
