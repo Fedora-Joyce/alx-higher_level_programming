a88ca3905c5ae973254469f0ad5ac720283cb03a
Acho Jay <101561702+Fedora-Joyce@users.noreply.github.com>
2022-12-01 13:02:34 +0100

    Add files via upload
    Updated

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    """
+    A function that returns a key with the biggest integer value.
+    """
+    if a_dictionary:
+        my_list = list(a_dictionary.keys())
+        score = 0
+        leader = ""
+        for i in my_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                leader = i
+<<<<<<< HEAD
+        return leader
+=======
+        return 
+>>>>>>> 8896dca0cd43afa33d1e71e1c99116de5abdcd14
