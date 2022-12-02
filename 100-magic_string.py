c7e690361d670d9370260d28cf9416a22a61342c
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-12-02 12:47:20 +0100

    updated

--- 100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(lis=[]):
-    lis.append('BestSchool')
-    return (', '.join(lis))
--- 101-locked_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" locked_class module """
-
-
-class LockedClass:
-    """ a class with no class or object attribute """
-
-    __slots__ = 'first_name'
